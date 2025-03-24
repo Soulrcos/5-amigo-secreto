df037340a2d4adf4bd819a41ed125f0740da1ca5
Marcos <marcos.pkinho@gmail.com>
2025-03-24 17:28:30 -0300

    Final

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const select = (identificador) => document.querySelector(identificador);
+let arrayAmigos = [];
+let nome = select('#nome-amigo');
+let listaAmigos = select('#lista-amigos');
+let sorteio = select('#lista-sorteio');
+
+window.addEventListener('keydown', (e) =>{
+        if (e.key == 'Enter') {
+            adicionar();
+        }
+})
+
+function adicionar(){
+    if (listaAmigos.innerText.length == 0 && nome.value.length > 0){
+        listaAmigos.innerText = nome.value;
+        arrayAmigos.push(nome.value);
+    } else if (nome.value.length > 0) {
+        listaAmigos.innerText += `, ${nome.value}`
+        arrayAmigos.push(nome.value);
+    }
+    nome.value = '';
+    
+}
+
+function sortear(){
+    if ((arrayAmigos.length)%2 == 0){
+        sorteio.innerHTML = '';
+        embaralha(arrayAmigos);
+        for(let i = 0; i<(arrayAmigos.length); i++){
+            if (i == arrayAmigos.length-1){
+                sorteio.innerHTML += `${arrayAmigos[i]} -> ${arrayAmigos[0]}<br>`
+            } else {
+                sorteio.innerHTML += `${arrayAmigos[i]} -> ${arrayAmigos[i+1]}<br>`
+            }
+        }
+        arrayAmigos = [];
+    }
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar(){
+    nome.value = '';
+    listaAmigos.innerText = '';
+    sorteio.innerHTML = '';
+}
